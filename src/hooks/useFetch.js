f55d4797ba8326073682c1b4878ff9f177fce175
maodesigns <mao.devv@gmail.com>
2023-02-21 12:07:00 +0100

    Updated

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -11,7 +11,7 @@ export const useFetch = () => {
   const getProducts = async () => {
     const response = await fetch(url);
     const data = await response.json();
-    setData(data);
+    paginate(data);
     setLoading(false);
   };
 
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,11 @@
-const paginate = () => {}
+const paginate = (followers) => {
+  const itemPerPage = 10;
+  const pages = Math.ceil(followers.length / itemPerPage);
+  const newFollowers = Array.from({ length: pages }, (_, index) => {
+    const start = index * itemPerPage;
+    return followers.slice(start, start + itemPerPage);
+  });
+  return newFollowers;
+};
 
-export default paginate
+export default paginate;
