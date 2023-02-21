9454f9327f1e1dc652dc2a765db7ee57f2de71de
maodesigns <mao.devv@gmail.com>
2023-02-21 23:12:02 +0100

    updated

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,6 +8,29 @@ import Follower from "../Follower/Follower";
 
 function App() {
   const { loading, data } = useFetch();
+  const [page, setPage] = useState(0);
+  const [followers, setFollowers] = useState([]);
+
+  const nextPage = () => {
+    setPage((oldPage) => {
+      let nextPage = oldPage + 1;
+      if (nextPage > data.length - 1) nextPage = 0;
+      return nextPage;
+    });
+  };
+
+  const prevPage = () => {
+    setPage((currPage) => {
+      let prevPage = currPage - 1;
+      if (prevPage < 0) prevPage = data.length - 1;
+      return prevPage;
+    });
+  };
+
+  useEffect(() => {
+    if (loading) return;
+    setFollowers(data[page]);
+  }, [loading, page]);
 
   return (
     <main>
@@ -17,10 +40,31 @@ function App() {
       </div>
       <section className='followers'>
         <div className='container'>
-          {data.map((follower) => (
+          {followers.map((follower) => (
             <Follower key={follower.id} {...follower} />
           ))}
         </div>
+        {!loading && (
+          <div className='btn-container'>
+            <button className='prev-btn' onClick={prevPage}>
+              prev
+            </button>
+            {data.map((_, index) => {
+              return (
+                <button
+                  className={`page-btn ${index === page ? "active-btn" : null}`}
+                  key={index}
+                  onClick={() => setPage(index)}
+                >
+                  {index + 1}
+                </button>
+              );
+            })}
+            <button className='next-btn' onClick={nextPage}>
+              next
+            </button>
+          </div>
+        )}
       </section>
     </main>
   );
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -11,7 +11,7 @@ export const useFetch = () => {
   const getProducts = async () => {
     const response = await fetch(url);
     const data = await response.json();
-    paginate(data);
+    setData(paginate(data));
     setLoading(false);
   };
 
